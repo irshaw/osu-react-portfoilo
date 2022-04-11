3f870d9f437bd172c4b21945b14c7fb03ce415a8
ian <ian.7.shaw@gmail.com>
2022-04-11 10:42:11 -0400

    upadting footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from "../src/components/navBar";
 import Header from "../src/components/header";
 import footer from "../src/components/footer";
-import projects from "../src/components/projects";
 import About from "../src/components/about";
 
 function App() {
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,6 +1,19 @@
+import React from "react"
+const footer = () => {
+    return (
+      <div className="section" id="footer">
+        <div className="container">
+        <h3> Contact </h3>
+        <h3> About </h3>
+        <h3> Projects </h3>
 
-
-
+         </div>
+         </div>
+    )}
+              
+          
+              
+  export default footer
 
 
 
